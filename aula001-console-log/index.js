26cd8113be8ad5e7620475a9c7548919088385be
Lucas Marques Lehm <159501964+lucaslehm@users.noreply.github.com>
2025-02-04 13:39:38 -0300

    aulas primarias

---
+++ aula001-console-log/index.js
@@ -0,0 +1,8 @@
+console.log('Lucas Lehm')
+console.log("Lucas Lehm")
+console.log(`Lucas Lehm`)
+
+console.log(7)
+console.log(1.7)
+
+console.log('Lucas', 22, 1.73)
---
+++ aula002-comentarios/index.js
@@ -0,0 +1,17 @@
+// Isso é um comentário
+console.log('Olá Mundo!')
+
+console.log('Linha 1') // Isso não vai ser exibido
+console.log('Linha 2')
+
+/* 
+
+Comentario de bloco
+
+*/
+
+console.log('Linha 3')
+// Isso também não vai ser exibido
+console.log('Linha 4')
+console.log('Linha 5')
+
